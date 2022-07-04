3ed262b84cabbf783d2702b68882c883ef04b340
ikvibhav <ikvibhav@gmail.com>
2022-07-04 08:40:56 +0200

    second commit

--- LinearRegression/linear_regression_v2.cpp
+++ LinearRegression/linear_regression_v2.cpp
@@ -13,12 +13,12 @@ int main(){
     double b0 = 10;
     double b1 = 10;
     double learning_rate = 0.0001;
-    int epochs = 5000;
+    int epochs = 500;
 
     //Training Phase
-    for(int i=0; i<epochs; i++){
+    for(uint32_t i=0; i<epochs; i++){
 
-        for(int index=0; index<dataset_length; index++){
+        for(uint32_t index=0; index<dataset_length; index++){
             double p = b0 + b1*x[index];
             index_error = p - y[index];
             b0 = b0 - learning_rate*index_error;
@@ -26,7 +26,7 @@ int main(){
             error_vector.push_back(index_error);
         }
 
-        std::cout << "Epoch " << i << " - b0 = "<< b0 << " b1 = "<< b1 << " index_error = " << index_error << std::endl;
+        std::cout << "Epoch " << +i << " - b0 = "<< b0 << " b1 = "<< b1 << " index_error = " << index_error << std::endl;
     
     }
 
