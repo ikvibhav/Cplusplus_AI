2ddffee1e0f453a44d260fae01b1c96a37723b03
Inna Kedege <vibhav.inna-kedege@capgemini.com>
2022-07-12 00:16:27 +0200

    12072022_code

--- LinearRegression/data_analysis.py
+++ LinearRegression/data_analysis.py
@@ -9,6 +9,7 @@ df_py = pd.read_csv("run_data_python.csv")
 
 df_comb = df_cpp.append(df_py, ignore_index=True)
 df_comb_mean = df_comb.groupby(['source','epoch'])['time(ms)'].mean().reset_index()
+print(df_comb_mean)
 sns.scatterplot(data=df_comb_mean, x='epoch', y='time(ms)' , hue='source').set(title='Epochs v/s Execution Time (Scatter)')
 sns.lmplot(data=df_comb_mean, x='epoch', y='time(ms)' , hue='source').set(title='Epoch v/s Execution Time (Line)')
 #sns.boxplot(data=df_comb, x='epoch', y='time(ms)' , hue='source')
